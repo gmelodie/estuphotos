fff7a3681013bb4a2aaee1cfbbcfaeeea16f3197
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2023-01-17 17:39:51 -0300

    add first working version (no users)

--- main.go
+++ main.go
@@ -18,18 +18,21 @@ import (
 	"gorm.io/gorm"
 )
 
+var log = logging.Logger("estuphotos")
+
 type Photo struct {
-	ID    uint   `gorm:"primarykey" json:"id"`
-	Name  string `json:"name"`
-	Owner *User  `json:"owner"`
-	CID   string `json:"cid"`
+	gorm.Model
+	Name   string `json:"name"`
+	UserID uint   `json:"user_id"`
+	CID    string `json:"cid"`
 }
 
 type User struct {
-	ID     uint   `gorm:"primarykey" json:"id"`
-	Handle string `json:"handle"`
-	Email  string `json:"email"`
-	ApiKey string `json:"apikey"`
+	gorm.Model
+	Handle string  `json:"handle"`
+	Email  string  `json:"email"`
+	ApiKey string  `json:"apikey"`
+	Photos []Photo `json:photos`
 }
 
 type API struct {
@@ -80,8 +83,7 @@ func (a *API) handleIndex(c echo.Context) error {
 // @Param    name  body      string  true  "name of the photo"
 // @Success  202        {object}  Photo
 // @Router   /photo [post]
-func (a *API) handlePhotoUpload(c echo.Context, u *User) error {
-	newPhoto := new(Photo)
+func (a *API) handlePhotoUpload(c echo.Context) error { // TODO: add user
 
 	// get file information from formdata
 	form, err := c.MultipartForm()
@@ -109,7 +111,15 @@ func (a *API) handlePhotoUpload(c echo.Context, u *User) error {
 	// upload file to edge-ur
 
 	fileNode, err := a.LightNode.Node.AddPinFile(context.Background(), fi, nil)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
 	size, err := fileNode.Size()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
 	content := core.Content{
 		Name:             filename,
 		Size:             int64(size),
@@ -123,10 +133,14 @@ func (a *API) handlePhotoUpload(c echo.Context, u *User) error {
 	a.LightNode.DB.Create(&content)
 
 	// add file to our own database
-	newPhoto.Owner = u
-	newPhoto.Name = filename
-	newPhoto.CID = content.Cid
-	a.DB.Save(&newPhoto)
+	newPhoto := Photo{
+		Name: filename,
+		CID:  content.Cid,
+	}
+	err = a.DB.Save(&newPhoto).Error
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
 
 	return c.JSON(http.StatusCreated, newPhoto)
 }
@@ -137,7 +151,7 @@ func (a *API) handlePhotoUpload(c echo.Context, u *User) error {
 // @Param    cid  query     string  true  "first name"
 // @Success  200        {object}  string
 // @Router   /photo/{cid} [get]
-func (a *API) handlePhotoDownload(c echo.Context, u *User) error {
+func (a *API) handlePhotoDownload(c echo.Context) error {
 	cid := c.Param("cid")
 
 	var photo Photo
@@ -166,27 +180,37 @@ func createDatabase() (*gorm.DB, error) {
 	return db, nil
 }
 
-// @title People API
-// @version 2.0
+// @title EstuPhotos
+// @version 1.0
 
 // @BasePath /
 // @schemes http
 func main() {
-	var log = logging.Logger("estuphotos")
 	var api API
 	var err error
+
+	logging.SetLogLevel("edge-ur", "debug")
+
+	viper.SetConfigFile(".env")
+	err = viper.ReadInConfig()
+	if err != nil {
+		log.Error(err)
+		panic(err)
+	}
 
 	// start light edge-ur node
 	ctx := context.Background()
 	api.LightNode, err = core.NewCliNode(&ctx)
 	if err != nil {
 		log.Fatal("could not start lightNode: %s", err)
+		panic(err)
 	}
 
 	// start database
 	api.DB, err = createDatabase()
 	if err != nil {
 		log.Fatal("could not create or open database: %s", err)
+		panic(err)
 	}
 
 	e := echo.New()
@@ -194,8 +218,10 @@ func main() {
 	e.GET("/", api.handleIndex)
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
-	e.POST("/photo", withUser(api.handlePhotoUpload))
-	e.GET("/photo/:cid", withUser(api.handlePhotoDownload))
+	// e.POST("/photo", withUser(api.handlePhotoUpload))
+	// e.GET("/photo/:cid", withUser(api.handlePhotoDownload))
+	e.POST("/photo", api.handlePhotoUpload)
+	e.GET("/photo/:cid", api.handlePhotoDownload)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
